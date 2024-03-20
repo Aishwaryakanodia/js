c90167faf6b26ee4f5a762766e4568e2d0fe87d2
Aishwaryakanodia <117978934+Aishwaryakanodia@users.noreply.github.com>
2024-03-20 06:23:40 +0000

    Updated

--- basic/conversion.js
+++ basic/conversion.js
@@ -1,19 +1,53 @@
-let score = "33abc"
-let isLoggedin = false
-let isLog= "Aishwarya"
-let isLog1= ""
-// const {score} = req.body
+// let score = "33abc"
+// let isLoggedin = false
+// let isLog= "Aishwarya"
+// let isLog1= ""
+// // const {score} = req.body
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let value = Number(isLoggedin)
-console.log(value)
+// let value = Number(isLoggedin)
+// console.log(value)
 
 
-let value1 = Boolean(isLog)
-console.log(value1)
+// let value1 = Boolean(isLog)
+// console.log(value1)
 
-let value2 = Boolean(isLog1)
-console.log(value2)
+// let value2 = Boolean(isLog1)
+// console.log(value2)
+
+
+
+// Operations
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 8)
+
+console.log(+true)
+console.log( true + true)
+console.log(false +true)
+console.log(true + 4)
+console.log(+ "")
