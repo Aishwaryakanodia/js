59ab6cb76ce78d31a3fa9191eea1d63b82440433
Aishwaryakanodia <117978934+Aishwaryakanodia@users.noreply.github.com>
2024-03-20 08:12:25 +0000

    Updated

---
+++ basic/nums_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// const balance =  new Number(1000)
+
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(score)
+// console.log(balance.toFixed(2))
+
+// const otherNumber =  876.9
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+
+// Maths
+
+
+
+console.log(Math)
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.94));
+console.log(Math.ceil(4.94));
+console.log(Math.min(2,6,7,8,9,9,0))
+console.log(Math.max(2,6,7,8,9,9,0))
+
+console.log(Math.random()*10 + 1)
+
+const min = 10
+const max= 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)))
